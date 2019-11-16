dcc8e8c7494bee6372dd26fd080db52b0cd3a6f9
Mate Bugar <bugar.matte@gmail.com>
2019-11-16 16:54:28 +0100

    files added

--- codereview/home.php
+++ codereview/home.php
@@ -15,6 +15,8 @@ $userRow=mysqli_fetch_array($res, MYSQLI_ASSOC);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
+
 <title>Welcome - <?php echo $userRow['userName' ]; ?></title>
 </head>
 <body >
